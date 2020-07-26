411c5f703d220fdff789ad2ebaeec7414bbb7698
Todd Brannon <toddqbrannon@gmail.com>
2020-07-26 18:17:41 -0500

    add flow control to handle error when file directory for import empty

--- import_df.py
+++ import_df.py
@@ -20,27 +20,57 @@ acct_files = sorted(glob(source + '*.CSV')) # Adapt this to your own needs (file
 # iterate over all files in 'acct_files' directory and create dataframe
 # if not csv files, make sure you use the appropriate pandas Input/Output method
 # https://pandas.pydata.org/pandas-docs/stable/reference/io.html
-df = pd.concat((pd.read_csv(file).assign(filename = file) 
-          for file in acct_files), ignore_index = True)
 
-# move the csv files to the archive folder
-for file in acct_files:
-        shutil.move(os.path.join(source,file),dest)
+filecount = len(acct_files)
 
-# Print the df to the terminal
-print(df)
+if filecount < 1:
+        # Print an error message to the terminal
+        print(" ")
+        print("*** SCRIPT ERROR MESSAGE ***")
+        print("No files found in the filepath: ")
+        print(source)
+        print("Please check the filepath and try again!")
+        print("*** END ERROR MESSAGE ***")
+        print(" ")
+else:        
+        # Create dataframe df from csv data
+        df = pd.concat((pd.read_csv(file).assign(filename = file) 
+        for file in acct_files), ignore_index = True)
 
-# Using sqlalchemy to make the db connection
-# User, pw, and db are being imported from databaseconfig file to mask credentials
-con = sq.create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
-                        .format(user=cfg.mysql["user"],
-                                pw=cfg.mysql["passwd"],
-                                db=cfg.mysql["db"]))
+        # move the csv files to the archive folder
+        for file in acct_files:
+            shutil.move(os.path.join(source,file),dest)
 
-# Insert the dataframe into the MySQL database table 'acct_activity'
-# use if_exists argument to 'replace' if you want to drop the table and replace
-# use if_exists argument to 'append' if you want to append new data to existing data
-df.to_sql("acct_activity", con, if_exists='append')
+        # Print the df to the terminal
+        print(df)
+
+        # Using sqlalchemy to make the db connection
+        # User, pw, and db are being imported from databaseconfig file to mask credentials
+        con = sq.create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                                .format(user=cfg.mysql["user"],
+                                        pw=cfg.mysql["passwd"],
+                                        db=cfg.mysql["db"]))
+
+        # Insert the dataframe into the MySQL database table 'acct_activity'
+        # use if_exists argument to 'replace' if you want to drop the table and replace
+        # use if_exists argument to 'append' if you want to append new data to existing data
+        df.to_sql("acct_activity", con, if_exists='append')
+
+        rowcount = format(len(df.index))
+        filecount = format(filecount)
+        db = format(cfg.mysql["db"])
+
+        print("SUCCESS: " + rowcount + " rows of data from " + filecount + " files imported to the " + db + " database.")
+
+
+        
+                
+                
+
+                
+
+
+
 
 
 
